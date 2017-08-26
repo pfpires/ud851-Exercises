b208847408deedac6c048cf9061f7552cfaa2345
Pedro Pires <pedro.pires@accenture.com>
2017-08-26 07:43:23 +0100

    Lesson 10, exercise 02 ...

--- Lesson10-Hydration-Reminder/T10.02-Exercise-CreateNotification/app/src/main/java/com/example/android/background/utilities/NotificationUtils.java
+++ Lesson10-Hydration-Reminder/T10.02-Exercise-CreateNotification/app/src/main/java/com/example/android/background/utilities/NotificationUtils.java
@@ -1,14 +1,46 @@
 package com.example.android.background.utilities;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+import android.support.v4.content.ContextCompat;
+
+import com.example.android.background.MainActivity;
+import com.example.android.background.R;
+
 /**
  * Utility class for creating hydration notifications
  */
 public class NotificationUtils {
+    private static final int WATER_REMINDER_PENDING_INTENT_ID = 123;
+    private static final int WATER_REMINDER_NOTIFICATION_ID = 33423;
 
     // TODO (7) Create a method called remindUserBecauseCharging which takes a Context.
     // This method will create a notification for charging. It might be helpful
     // to take a look at this guide to see an example of what the code in this method will look like:
     // https://developer.android.com/training/notify-user/build-notification.html
+    public void remindUserBecauseCharging(Context context) {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
+                .setColor(ContextCompat.getColor(context,R.color.colorPrimary))
+                .setSmallIcon(R.drawable.ic_drink_notification)
+                .setLargeIcon(largeIcon(context))
+                .setContentTitle(context.getString(R.string.charging_reminder_notification_title))
+                .setContentText(context.getString(R.string.charging_reminder_notification_body))
+                .setStyle(new NotificationCompat.BigTextStyle().bigText(context.getString(R.string.charging_reminder_notification_body)))
+                .setDefaults(Notification.DEFAULT_VIBRATE)
+                .setContentIntent(contentIntent(context))
+                .setAutoCancel(true);   // auto cancel - Cancels the notification when the users clicks on it
+
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(WATER_REMINDER_NOTIFICATION_ID,builder.build());
+    }
         // TODO (8) In the remindUser method use NotificationCompat.Builder to create a notification
         // that:
         // - has a color of R.colorPrimary - use ContextCompat.getColor to get a compatible color
@@ -31,6 +63,10 @@ public class NotificationUtils {
     // TODO (1) Create a helper method called contentIntent with a single parameter for a Context. It
     // should return a PendingIntent. This method will create the pending intent which will trigger when
     // the notification is pressed. This pending intent should open up the MainActivity.
+    private PendingIntent contentIntent(Context context) {
+        Intent intent = new Intent(context, MainActivity.class);
+        return PendingIntent.getActivity(context,WATER_REMINDER_PENDING_INTENT_ID,intent,PendingIntent.FLAG_UPDATE_CURRENT);
+    }
         // TODO (2) Create an intent that opens up the MainActivity
         // TODO (3) Create a PendingIntent using getActivity that:
             // - Take the context passed in as a parameter
@@ -44,6 +80,9 @@ public class NotificationUtils {
 
     // TODO (4) Create a helper method called largeIcon which takes in a Context as a parameter and
     // returns a Bitmap. This method is necessary to decode a bitmap needed for the notification.
+    private static Bitmap largeIcon(Context context) {
+        return BitmapFactory.decodeResource(context.getResources(), R.drawable.ic_local_drink_black_24px);
+    }
         // TODO (5) Get a Resources object from the context.
         // TODO (6) Create and return a bitmap using BitmapFactory.decodeResource, passing in the
         // resources object and R.drawable.ic_local_drink_black_24px
